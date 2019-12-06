3eb814e999b9438ef24e65586580556b62f89b31
Natanael Neto <natanael.eneas2018@hotmail.com>
2019-12-06 14:02:52 -0300

    add menu sound

--- TheGrid/main.js
+++ TheGrid/main.js
@@ -2,6 +2,11 @@ var divMenu = document.getElementById("divMenu");
 var divGame = document.getElementById("divGame");
 var canvas = document.getElementById("myCanvas");
 
+var mouseHover = new Howl({
+    src: ['cursor.wav'],
+    volume: 0.5
+});
+
 divMenu.style.top = "-1000px";
 divMenu.style.top = 0;
 divGame.style.top = "-1000px";
